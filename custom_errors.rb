b8edfd39cec15815c03cbd7cfb24261631ce87b6
RebGov <becci.govert@gmail.com>
2018-07-22 20:54:49 -0500

    code and learn error msg

--- custom_errors.rb
+++ custom_errors.rb
@@ -5,10 +5,26 @@ class Person
     @name = name
   end
 
+
+
   def get_married(person)
     self.partner = person
+    if person.class != Person
+      begin
+     raise PartnerError
+   rescue PartnerError => error
+     puts error.message
+   end
+   else
     person.partner = self
   end
+end
+
+  class PartnerError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+end
 
 end
 
@@ -17,5 +33,23 @@ beyonce.get_married("Jay-Z")
 puts beyonce.name
 
 
+#1.defining the custom error class
+  # class PartnerError < StandardError
+  # end
 
-
+#2. raising our custom error
+  # if person.class != Person
+  #      raise PartnerError
+  #    else
+##custom error handling-resquing
+# 1. writing a custom error msg
+  # class PartnerError < StandardError
+  #   def message
+  #     "you must give the get_married method an argument of an instance of the person class!"
+  #   end
+  # end
+#2. implementing the RESCUE
+  # begin
+  #   raise YourCustomError
+  # rescue YourCustomError
+  # end
